7e4ce831d6b2aec34e3fe3ac4aab0753df2b4daf
Yan Lima Barbosa <yanbr763@gmail.com>
2025-04-19 11:22:50 -0300

    feat: implement Serie class to represent TV series with attributes and methods for data handling

---
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -0,0 +1,92 @@
+package br.com.alura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+import java.util.OptionalDouble;
+
+public class Serie {
+    private String titulo;
+    private Integer totalTemporadas;
+    private Double avaliacao;
+    private Categoria genero;
+    private String atores;
+    private String sinpose;
+    private String poster;
+
+    public Serie(DadosSerie dadosSerie){
+        this.titulo = dadosSerie.titulo();
+        this.totalTemporadas = dadosSerie.totalTemporadas();
+        this.avaliacao = OptionalDouble.of(Double.valueOf(dadosSerie.avaliacao())).orElse(0);
+        this.genero = Categoria.fromString(dadosSerie.genero().split(",")[0].trim());
+        this.atores = dadosSerie.atores();
+        this.poster = dadosSerie.poster();
+        this.sinpose = dadosSerie.sinpose();
+    }
+
+    public Integer getTotalTemporadas() {
+        return totalTemporadas;
+    }
+
+    public void setTotalTemporadas(Integer totalTemporadas) {
+        this.totalTemporadas = totalTemporadas;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(Double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public Categoria getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Categoria genero) {
+        this.genero = genero;
+    }
+
+    public String getAtores() {
+        return atores;
+    }
+
+    public void setAtores(String atores) {
+        this.atores = atores;
+    }
+
+    public String getSinpose() {
+        return sinpose;
+    }
+
+    public void setSinpose(String sinpose) {
+        this.sinpose = sinpose;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+    @Override
+    public String toString() {
+        return  "genero=" + genero +
+                ", titulo='" + titulo + '\'' +
+                ", totalTemporadas=" + totalTemporadas +
+                ", avaliacao=" + avaliacao +
+                ", atores='" + atores + '\'' +
+                ", sinpose='" + sinpose + '\'' +
+                ", poster='" + poster + '\'';
+    }
+}
