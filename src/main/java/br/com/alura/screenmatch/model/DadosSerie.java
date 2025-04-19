1e1e32e02c8bf89d04274faa87c3e789e2329b55
Yan Lima Barbosa <yanbr763@gmail.com>
2025-04-19 10:40:40 -0300

    feat: extend DadosSerie model with additional fields for genre, actors, plot, and poster

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -6,5 +6,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
-                         @JsonAlias("imdbRating") String avaliacao) {
+                         @JsonAlias("imdbRating") String avaliacao,
+                         @JsonAlias("Genre") String genero,
+                         @JsonAlias("Actors") String atores,
+                         @JsonAlias("Plot") String sinpose,
+                         @JsonAlias("Poster") String poster
+
+) {
 }
