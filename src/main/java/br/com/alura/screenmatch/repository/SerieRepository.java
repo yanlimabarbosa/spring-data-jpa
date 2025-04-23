187dbb019ca4c8d8802bf73abe333941ad2e0531
Yan Lima Barbosa <yanbr763@gmail.com>
2025-04-23 05:50:19 -0300

    feat: add method to SerieRepository for case-insensitive search by series title

--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -3,6 +3,9 @@ package br.com.alura.screenmatch.repository;
 import br.com.alura.screenmatch.model.Serie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
-public interface SerieRepository extends JpaRepository<Serie, UUID> {}
+public interface SerieRepository extends JpaRepository<Serie, UUID> {
+    Optional<Serie> findByTituloContainingIgnoreCase(String nomeSerie);
+}
