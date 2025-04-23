4986098c2074e477c1ef47d6878cdc574bef1f61
Yan Lima Barbosa <yanbr763@gmail.com>
2025-04-23 04:12:38 -0300

    feat: update Serie class to establish one-to-many relationship with Episodio and ensure proper association during episodios assignment

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -31,7 +31,7 @@ public class Serie {
     
     private String poster;
 
-    @Transient
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie() {}
@@ -115,6 +115,7 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e->e.setSerie(this));
         this.episodios = episodios;
     }
 
@@ -126,6 +127,7 @@ public class Serie {
                 ", avaliacao=" + avaliacao +
                 ", atores='" + atores + '\'' +
                 ", sinpose='" + sinpose + '\'' +
-                ", poster='" + poster + '\'';
+                ", poster='" + poster + '\'' +
+                ", episodios='" + episodios + '\'';
     }
 }
